88807806845281d8486283aecc6d0ca73296bb63
Joaquin Correa <jecorrea@protonmail.com>
2021-03-22 22:38:21 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,7 @@ def move(board, index, token = "X")
 end
 
 def turn(board)
-  puts "Please enter 1-9: "
+  puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)

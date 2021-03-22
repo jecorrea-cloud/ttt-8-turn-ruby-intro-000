d38014d9763b0168dfd831473a31c0c2fa41091c
Joaquin Correa <jecorrea@protonmail.com>
2021-03-22 21:58:49 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,3 +11,19 @@ end
 def input_to_index (user_input)
   converted_input = user_input.to_i-1
 end
+
+def valid_move?(board, index)
+  if !(position_taken(board, index)) && index.between?(0,8)
+    return true
+  else
+    return false
+end
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  else
+    return true
+end
